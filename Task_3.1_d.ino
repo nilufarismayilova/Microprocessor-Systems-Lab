54ef68d9d2ca6b9a672af9814b48107fea3ba7f6
nilufarismayilova <nilufarismayilova@gmail.com>
2025-10-06 23:36:35 +0400

    Update Task_3.1_d.ino

--- Task_3.1_d.ino
+++ Task_3.1_d.ino
@@ -2,6 +2,7 @@ void setup() {
   DDRB = 0b00100000 ;   // Sets the LED (PB5) as an output
 }
 
+void loop() {
   PORTB = (1 << 5);    // Turns LED on by shifting 1 left by 5 places (0b00100000) 
   delay(500);
   PORTB = (1 << 0);     // Turns the LED off by shifting 1 by 0 places (0b00000001), meaning bit 5 is 0.
